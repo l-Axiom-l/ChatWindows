75b2977785da862da65fe176b28cd65a40b955d8
l_Axiom_l <axiom@mail.gmx>
2022-01-19 17:26:48 +0100

    Projektdateien hinzufügen.

---
+++ ChatWindows/Chat.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatProgram
+{
+    public class Chat
+    {
+        //https://stackoverflow.com/questions/56407836/connecting-to-local-ip-address-c-sharp-sockets
+        //Maybe it works better
+
+        public List<string> ChatHistory = new List<string>();
+
+        public string ServerIPAddress = "localhost"; //It works with 127.0.0.1
+        public string ClientIPAddress = "localhost";
+
+        public string DeviceType;
+
+        Socket Sender;
+
+        public void ChangeServerIP(string address)
+        {
+            ServerIPAddress = address;
+        }
+
+        public void ChangeClientIP(string address)
+        {
+            ClientIPAddress = address;
+        }
+
+        public void SaveChatHistory()
+        {
+
+        }
+
+        public void LoadChatHistory()
+        {
+
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(message + "<EOF>");
+            Sender.Send(msg);
+        }
+
+        public void Update()
+        {
+            if (ChatHistory == null)
+                return;
+
+            Console.Clear();
+            foreach(string temp in ChatHistory)
+            {
+                Console.WriteLine(temp);
+            }
+        }
+
+        public void StartClient()
+        {
+            IPHostEntry host = Dns.GetHostEntry(ClientIPAddress);
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+            Sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Sender.Connect(remoteEP);
+            byte[] msg = Encoding.ASCII.GetBytes("Test" + "<EOF>");
+            Sender.Send(msg);
+            //Sender.Shutdown(SocketShutdown.Both);
+            //Sender.Close();
+        }
+
+        public void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry(ServerIPAddress);
+            IPAddress ipAddress = host.AddressList[0];
+            Console.WriteLine(ipAddress.ToString());
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(localEndPoint);
+            socket.Listen(10);
+            socket = socket.Accept();
+            LOOP:
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = socket.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+            ChatHistory.Add(data);
+            goto LOOP;
+        }
+    }
+
+    public class ConnectionListener
+    {
+        public static void StartServer(object chat)
+        {
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            
+
+            try
+            {
+
+                // Create a Socket that will use Tcp protocol
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // A Socket must be associated with an endpoint using the Bind method
+                listener.Bind(localEndPoint);
+                // Specify how many requests a Socket can listen before it gives Server busy response.
+                // We will listen 10 requests at a time
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+
+                // Incoming data from the client.
+                string data = null;
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received : {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    public class ConnectionClient
+    {
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Connect to a Remote server
+                // Get Host IP Address that is used to establish a connection
+                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+                // If a host has multiple addresses, you will get a list of addresses
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    // Connect to Remote EndPoint
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ ChatWindows/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace ChatProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Chat chat = new Chat();
+            LOOP:
+            Console.WriteLine("What do you want to do?");
+            switch (Console.ReadLine())
+            {
+                default: goto LOOP;
+
+                case "Server":
+                    Console.WriteLine("Type the ServerIP");
+                    chat.ChangeServerIP(Console.ReadLine());
+                    Console.WriteLine("Activating Server");
+                    Thread threadServer = new Thread(chat.StartServer);
+                    threadServer.Start();
+                    goto LOOP;
+
+
+                case "Client":
+                    Console.WriteLine("Type the ClientIP");
+                    chat.ChangeClientIP(Console.ReadLine());
+                    Console.WriteLine("Activating Client");
+                    Thread threadClient = new Thread(chat.StartClient);
+                    threadClient.Start();
+                    goto LOOP;
+
+                case "Send":
+                    Console.WriteLine("Type your Message");
+                    chat.SendMessage(Console.ReadLine());
+                    goto LOOP;
+
+                case "SetIP":
+                    Console.WriteLine("Type the ServerIP");
+                    chat.ChangeServerIP(Console.ReadLine());
+                    Console.WriteLine("Type the ClientIP");
+                    chat.ChangeClientIP(Console.ReadLine());
+                    goto LOOP;
+
+                case "GetIP":
+                    string hostName = Dns.GetHostName(); // Retrive the Name of HOST
+                    string myIP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+                    Console.WriteLine(myIP);
+                    goto LOOP;
+
+                case "Update":
+                    chat.Update();
+                    goto LOOP;
+
+                case "Quit":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
